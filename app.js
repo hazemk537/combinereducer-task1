073c67a4bb43f32039528f45cfd103e3cc862bd7
HazemShehawy <Hazemk537@gmail.com>
2023-05-22 18:06:23 +0300

    Mission 001 complete

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
-import * as Redux from 'redux' //es6 valid
 
+// import * as Redux from '/redux' //es6 valid //use type="module" in html not works
+//use cdn script link then app.js script in html
 const btn1=document.querySelector('.btn1')
 const btn2=document.querySelector('.btn2')
 const box1=document.querySelector('.box1')
 const box2=document.querySelector('.box2')
 
-
-//combine reducers
-const rootReducer =Redux.combineReducers( {
-inc1:Reducer1,
-incr2:Reducer2
-})
 //reducer receive dispatches 
 const Reducer1=(initialState1="50%",action)=>{ //should return value whatever the if test
-    if (action==="inc1") 
+    if (action.type==="inc1") 
       return "100%"
     return "50%"
         
 }
 const Reducer2=(initialState2="50%",action)=>{  //should return value whatever the if test
-    if (action==="inc2")
+    if (action.type==="inc2")
       return  "100%"
     return "50%"
 
 }
+//combine reducers
+const rootReducer = Redux.combineReducers( {
+    inc1:Reducer1,
+    inc2:Reducer2
+    })
+
 //create store and subscribe
 const firstStore=Redux.createStore(rootReducer)//not 'rootreducer'
-firstStore.subscribe(setStyle())
 
 //event listeners to send/dispatch actions
 btn1.addEventListener('click',function(){
-    firstStore.dispatch({type:"inc2"})
+    firstStore.dispatch({type:"inc1"})
 })
 btn2.addEventListener('click',function(){
-    firstStore.dispatch({type:"inc1"})
-    console.log("test");
+    firstStore.dispatch({type:"inc2"})
 })
 
 //styling
-const setStyle =()=>{
-    box1.style.width=firstStore.Reducer1
-    box2.style.width=firstStore.Reducer2
+const setStyle = () =>{
+    box1.style.width=firstStore.getState().inc1
+    box2.style.width=firstStore.getState().inc2
+    console.log(firstStore.getState().inc1)
+    console.log(firstStore.getState().inc2)
+
+
 }
+
 setStyle()
+
+firstStore.subscribe(setStyle)
+
+
